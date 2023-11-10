5eac597a013bcc4cc534ee413a2b33c8241c9107
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2023-11-09 21:35:20 -0800

    Added logic to export js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Navbar } from './components/layout/Navbar';
-import { Landing } from './components/layout/Landing';
-import { Register } from './components/auth/Register';
-import { Login } from './components/auth/Login';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
 
 const App = () => (
@@ -11,9 +12,9 @@ const App = () => (
     <Fragment>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
+        <Route exact path='/' element={<Landing />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/login' element={<Login />} />
       </Routes>
     </Fragment>
   </Router>
